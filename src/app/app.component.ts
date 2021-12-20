2157679650ee57783436f0fb5385944c6560b20d
SalomaoFerreira <salomao1991@gmail.com>
2021-12-20 15:12:12 -0500

    Working with component styles, inline and external css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+
+  //external style
+  styleUrls: ['./app.component.css'] 
+
+  //inline style
+  // styles: [`
+  //   h3 {
+  //     color: dodgerblue;
+  //   }
+  //   `]
 })
 export class AppComponent {
 }
